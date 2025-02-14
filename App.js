414f5a45e342f6a6413dd8d62f039fcdb1303dd0
LeoYaMM <y.leobardomartinez@gmail.com>
2025-02-13 20:53:49 -0600

    Enhance App component with logo, input fields, and button for user interaction

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+  Pressable,
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>¡Hola Mundo desde React Native!</Text>
+      <Image style={styles.logo} source={require('./img/QuetzAI.png')} />
+      <Text style={styles.textLogo1}>
+        Quetz
+        <Text style={styles.textLogo2}>AI</Text>
+      </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nombre"
+        keyboardType="default"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Edad"
+        keyboardType="numeric"
+      />
+      <Pressable
+        style={({pressed}) => [
+          {
+            backgroundColor: pressed ? 'black' : '#fff',
+            color: pressed ? '#fff' : 'black',
+          },
+          styles.button,
+        ]}>
+        <Text style={styles.textInPlace}>INICIAR</Text>
+      </Pressable>
     </View>
   );
 }
@@ -16,8 +47,42 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#f5f5f5',
   },
-  text: {
+  textLogo1: {
     fontSize: 24,
     color: '#333',
+    fontWeight: 'bold',
+  },
+  textLogo2: {
+    fontSize: 24,
+    color: '#008000',
+    fontWeight: 'bold',
+  },
+  textInPlace: {
+    fontSize: 18,
+    color: '#333',
+    fontWeight: 'bold',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    alignContent: 'center',
+  },
+  input: {
+    height: 40,
+    width: 150,
+    margin: 10,
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 5,
+  },
+  button: {
+    padding: 10,
+    borderRadius: 0,
+    borderWidth: 2,
+    fontSize: 18,
+    height: 45,
+    width: 125,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
